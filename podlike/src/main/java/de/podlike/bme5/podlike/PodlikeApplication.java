a8065ef492a38058f42538e56e492f6eb48abcbf
Lo0rion <mauriceglaser@googlemail.com>
2020-11-12 14:38:46 +0100

    Initial Setup Upload

---
+++ podlike/src/main/java/de/podlike/bme5/podlike/PodlikeApplication.java
@@ -0,0 +1,13 @@
+package de.podlike.bme5.podlike;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PodlikeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PodlikeApplication.class, args);
+    }
+
+}
---
+++ podlike/src/test/java/de/podlike/bme5/podlike/PodlikeApplicationTests.java
@@ -0,0 +1,13 @@
+package de.podlike.bme5.podlike;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PodlikeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
